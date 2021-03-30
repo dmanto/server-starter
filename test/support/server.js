c928d2de817a389c3535fbae01138fb10586a8c5
Daniel Mantovani <dmanto@gmail.com>
2021-03-30 20:03:04 -0300

    wait for port to start listening

--- test/support/server.js
+++ test/support/server.js
@@ -1,9 +1,13 @@
-'use strict';
+'use strict'
 
-const http = require('http');
+const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello World!')
 });
-server.listen(process.env.TEST_SERVER_STARTER_PORT ? process.env.TEST_SERVER_STARTER_PORT : { fd: 3 });
+(() => new Promise(resolve => {
+  setTimeout(
+    () => resolve(server.listen(process.env.TEST_SERVER_STARTER_PORT ? process.env.TEST_SERVER_STARTER_PORT : { fd: 3 })),
+    14000)
+}))()
